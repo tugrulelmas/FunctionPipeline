5e28a77b572b4b6be51e1be03c59586187d26656
Tugrul Elmas <tugrulelmas@gmail.com>
2020-03-05 14:43:33 +0300

    v3

--- GetName.cs
+++ GetName.cs
@@ -13,17 +13,13 @@ namespace FunctionPipeline
     public static class GetName
     {
         [FunctionName("GetName")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Admin, "get", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
