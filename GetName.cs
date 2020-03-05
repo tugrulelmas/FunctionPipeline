fd33fb6d64381e25c148871bc06e9fd7404ad633
Tugrul Elmas <tugrulelmas@gmail.com>
2020-03-05 14:38:24 +0300

    v2

--- GetName.cs
+++ GetName.cs
@@ -27,7 +27,7 @@ namespace FunctionPipeline
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. v2";
 
             return new OkObjectResult(responseMessage);
         }
